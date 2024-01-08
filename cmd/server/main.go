56bd21d09a6899694178dc646ccc741abf235569
dieofcode <fr0stsone878@gmail.com>
2024-01-08 23:07:22 +0500

    add logger for requests

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/DieOfCode/go-alert-service/internal/configuration"
 	"github.com/DieOfCode/go-alert-service/internal/handler"
+	log "github.com/DieOfCode/go-alert-service/internal/logger"
 	"github.com/DieOfCode/go-alert-service/internal/storage"
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog"
 )
 
 func main() {
 
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
 	config := configuration.ServerConfiguration()
 	memStorage := storage.NewMemStorage()
 	handler := handler.NewHandler(memStorage)
 
 	router := chi.NewRouter()
-
 	router.Route("/", func(r chi.Router) {
+		r.Use(middleware.RequestLogger(&log.LogFormatter{Logger: &logger}))
+		r.Use(middleware.Recoverer)
 		r.MethodFunc(http.MethodPost, "/update/{type}/{name}/{value}", handler.HandleUpdateMetric)
 		r.MethodFunc(http.MethodGet, "/value/{type}/{name}", handler.HandleGetMetricByName)
 		r.MethodFunc(http.MethodGet, "/", handler.HandleGetAllMetrics)
@@ -27,6 +32,6 @@ func main() {
 	err := http.ListenAndServe(config.ServerAddress, router)
 
 	if err != nil {
-		fmt.Println("Ошибка запуска сервера:", err)
+		logger.Fatal().Err(err).Msg("Server start error")
 	}
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,12 @@ go 1.21.3
 require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.0.10
+	github.com/rs/zerolog v1.31.0
+)
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
