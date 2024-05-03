a10a5b59b2017a9594fada34750a39ef60932a44
dieofcode <fr0stsone878@gmail.com>
2024-05-03 09:08:25 +0400

    update dependency

--- db/0_init.up.sql
+++ db/0_init.up.sql
@@ -2,7 +2,7 @@
 CREATE TYPE metric_type AS ENUM ('counter', 'gauge');
 CREATE TABLE metrics (
     id VARCHAR NOT NULL,
-    type VARCHAR NOT NULL,
+    type metric_type NOT NULL,
     delta BIGINT,
     value DOUBLE PRECISION,
     UNIQUE (id, type)
